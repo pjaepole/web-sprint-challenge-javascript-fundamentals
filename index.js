ed5c12886d9a9df58097dadf79803b7c48c73e72
Jaehun Park <pjaehun4122@gmail.com>
2021-08-20 00:46:13 -0700

    finised task 1 and 2

--- index.js
+++ index.js
@@ -16,7 +16,8 @@ function myFunction() {
 myFunction();
 
 //🚀🚀🚀 ⬇️ 📝 Explanation ⬇️ 📝 🚀🚀🚀: 
-
+// nestedFunction can access the variable internal due to the closure. closure happens when a inner function in this case nested 
+// function can reach out to outer function to grab a value that is defined in the outerfunction.
 
 
 
@@ -28,12 +29,12 @@ myFunction();
     
 For example, `summation(4)` should return 10 because 1+2+3+4 is 10. Note, you may use a for loop for this function if you wish */
 
-function summation(/*Your Code Here*/) {
-  /*Your Code Here*/
-
-  }
+function summation(number){
+  let sum=0;
+  for(let i=0; i<=number; i++){sum= sum+ i}
+  return sum;
+}
  
-
 // 🦁🦁🦁 Topic 2: ADVANCED Array Methods 🦁🦁🦁
 // Given this zoo data from around the United States, follow the instructions below. Use the specific array methods in the requests below to solve the problems.
 
